da5de26a2a7094423627f08d6ccebc9462859f26
TonyDMorris <anthonymorris86@googlemail.com>
2023-11-24 13:12:17 +0000

    chema

--- cms/src/api/installation/controllers/installation.js
+++ cms/src/api/installation/controllers/installation.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * installation controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::installation.installation');
+module.exports = createCoreController(
+  "api::installation.installation",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const user = ctx.state.user;
+      ctx.query = {
+        ...ctx.query,
+        filters: {
+          ...ctx.query.filters,
+          username: user.username,
+        },
+      };
+
+      const { data, meta } = await super.find(ctx);
+
+      // some more custom logic
+      meta.date = Date.now();
+
+      return { data, meta };
+    },
+  })
+);
--- cms/src/api/installation/routes/installation.js
+++ cms/src/api/installation/routes/installation.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * installation router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::installation.installation');
+module.exports = createCoreRouter("api::installation.installation", {
+  routes: [
+    {
+      method: "POST",
+      path: "/installation",
+      handler: "installation.handleInstallationWebhook",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+});
